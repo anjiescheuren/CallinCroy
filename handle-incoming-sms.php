acd0b8741c86f0f08097b852fe290e1c59aa9268
Anjie Scheuren <emailanjie@gmail.com>
2016-08-08 17:30:50 -0400

    update sms

--- handle-incoming-sms.php
+++ handle-incoming-sms.php
@@ -1,6 +1,6 @@
 <?php
     header("content-type: text/xml");
-    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+    echo "<?xml version="1.0" encoding="UTF-8"?>";
 ?>
 <Response>
     <Message>Croy & The Boys Album Release November 7, 2016</Message>
