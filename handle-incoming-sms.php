e783f413627529f51a7d6819c78cb5a07a30204a
Anjie Scheuren <emailanjie@gmail.com>
2016-08-10 17:30:11 -0500

    fixed sms

--- handle-incoming-sms.php
+++ handle-incoming-sms.php
@@ -3,5 +3,5 @@
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
 <Response>
-    <Message>Croy and The Boys Album Release September 16, 2016</Message>
+    <Message>Howdy! Croy and The Boys' Debut Album "Hey Come Back", produced by Adrian Quesada, will be released September 16, 2016. Call this number for a sneak peak!</Message>
 </Response>
