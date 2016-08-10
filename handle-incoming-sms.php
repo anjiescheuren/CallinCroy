120ca33b86150cd176f9b45f1719cf04b39b4213
Anjie Scheuren <emailanjie@gmail.com>
2016-08-10 17:52:00 -0500

    sms update:

--- handle-incoming-sms.php
+++ handle-incoming-sms.php
@@ -3,5 +3,5 @@
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
 <Response>
-    <Message>Howdy! Croy and The Boys' Debut Album "Hey Come Back", produced by Adrian Quesada, will be released September 16, 2016. Call this number for a sneak peak!</Message>
+    <Message>Howdy! "Hey Come Back", Croy and The Boys' debut album produced by Adrian Quesada will be released September 16, 2016. Call this number for a sneak peak!</Message>
 </Response>
