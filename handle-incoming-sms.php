2d30a90aec6e858a665775e8056692a9d066aa6b
Anjie Scheuren <emailanjie@gmail.com>
2016-08-10 17:24:27 -0500

    moved play inside gather

--- handle-incoming-sms.php
+++ handle-incoming-sms.php
@@ -3,5 +3,5 @@
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
 <Response>
-    <Message>Croy and The Boys Album Release November 7, 2016</Message>
+    <Message>Croy and The Boys Album Release September 16, 2016</Message>
 </Response>
